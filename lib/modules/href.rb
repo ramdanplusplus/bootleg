45053eeb8ae6dcc281ea6954fafaab9e8371e7b0
dadan41 <dadan@41studio.com>
2013-01-31 11:55:12 +0700

    update href module part 1

--- lib/modules/href.rb
+++ lib/modules/href.rb
@@ -2,23 +2,27 @@ module Href
   def all
     pages = []
     if count
-      count.times { |nr| pages << url + nr.to_s }
+      count.times { |nr| pages << url + (nr + 1).to_s }
     end
+    pages.map{|pa| puts pa}
     pages
   end
 
   private
 
   def count
-    # links = self.links.flatten.compact.select { |link| link.text.size < 3 and link.text =~ /\d/  }
-    links = self.links.flatten.compact.select { |link| link.text.size < 3}
-    #links = self.links.flatten.compact.select { |link| link.text.include?("http://www.moviefone.com/theater/")  }
-    links.map{|a| puts "#{a.text} ====== #{a.uri.to_s}" }
-    links.empty? ? nil : links.last.text.to_i
+    # # links = self.links.flatten.compact.select { |link| link.text.size < 3 and link.text =~ /\d/  }
+    # links = self.links.flatten.compact.select { |link| link.text.size < 3}
+    # #links = self.links.flatten.compact.select { |link| link.text.include?("http://www.moviefone.com/theater/")  }
+    # links.map{|a| puts "#{a.text} ====== #{a.uri.to_s}" }
+    # links.empty? ? nil : links.last.text.to_i
+    self.search("span").css(".page-status").first.text.last.to_i
+
   end
 
   def url
-    self.uri.to_s + '?page='
+    #self.uri.to_s + '?page='
+    "http://www.moviefone.com/showtimes/albany-ny/12201/theaters?page="
   end
 end
 
