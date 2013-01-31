625afac3678e1ae24e4a165d2cfe3910110282bf
dadan41 <dadan@41studio.com>
2013-01-31 17:02:50 +0700

    fix href module

--- lib/modules/href.rb
+++ lib/modules/href.rb
@@ -11,18 +11,11 @@ module Href
   private
 
   def count
-    # # links = self.links.flatten.compact.select { |link| link.text.size < 3 and link.text =~ /\d/  }
-    # links = self.links.flatten.compact.select { |link| link.text.size < 3}
-    # #links = self.links.flatten.compact.select { |link| link.text.include?("http://www.moviefone.com/theater/")  }
-    # links.map{|a| puts "#{a.text} ====== #{a.uri.to_s}" }
-    # links.empty? ? nil : links.last.text.to_i
+    # links = self.links.flatten.compact.select { |link| link.text.size < 3 and link.text =~ /\d/  }
     self.search("span").css(".page-status").first.text.last.to_i
-
   end
 
   def url
-    #self.uri.to_s + '?page='
-    #"http://www.moviefone.com/showtimes/albany-ny/#{Manager.zipcode}/theaters?page="
     self.uri.to_s + '?page='
   end
 end
